dbc1e9c4897d57f8180b89a69879299cd838fa23
senghuy <senghuy001@gmail.com>
2022-11-28 16:44:18 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/jparelationshipkt/JpaRelationshipKtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jparelationshipkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaRelationshipKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaRelationshipKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jparelationshipkt/JpaRelationshipKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jparelationshipkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaRelationshipKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
